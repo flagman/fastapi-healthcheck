ed775d29801bd7566cdea4fa4525b5e50eac823b
Pavel Malay <flagmansupport@gmail.com>
2020-05-27 18:48:17 +0700

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fastapi-healthcheck-flagman",
-    version="0.0.2",
+    version="0.0.3",
     author="Pavel Malay",
     author_email="flagmansupport@gmail.com",
     description="A small healthcheck package for adding /healthcheck routes to FastAPI projects",
